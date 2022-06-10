f186681cfca984fa4d7771bee77d2f002cef31d7
Jhonatan-Ordonez <jhoeduardonez@gmail.com>
2022-06-09 21:19:25 -0500

    Added download cv option

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../styles/home.css";
 import profile from "../styles/images/profile2.jpg";
+import cv from "../assets/cv.pdf";
 
 const Home = () => {
   return (
@@ -9,7 +10,11 @@ const Home = () => {
       <h1 className="h2-style">Jhonatan</h1>
       <p className="p-style">Web developer & designer </p>
 
-      <button className="download-cv">download CV</button>
+      <button className="download-cv">
+        <a href={cv} download>
+          download CV
+        </a>
+      </button>
       <button className="lets-talk ">Lets talk</button>
 
       <div>
