c29f1d7250ae0b3d2d6d3722d545f89f20a4128a
Jhonatan-Ordonez <jhoeduardonez@gmail.com>
2022-06-12 23:21:22 -0500

    Set grid for columns in skills section

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -10,39 +10,39 @@ import { motion } from "framer-motion";
 const Skills = () => {
   // const getNames = names.map((name) => <h5>{name}</h5>);
 
-  // const skillsImg = images.map((image) => <img src={image} alt="Imgs"></img>);
+  // const [changeImg, setChangeImg] = useState('');
 
-  // const [changeImg, setChangeImg] = useState(skillsImg);
+  // const getImgs = images.map(image => setChangeImg(image))
 
   return (
-    <section id="skills" className="section-skills">
+    <section id="skills">
       <h5>Skills that I have</h5>
       <h2>Technologies</h2>
 
-      <div className="btn-left">
-        <a href="#css">
+      <div className="section-skills">
+        <a className="btn-left" href="css">
           <GrFormPrevious />
         </a>
-      </div>
 
-      <motion.div className="container-skills">
-        <motion.div
-          className="slider"
-          drag="x"
-          dragConstraints={{ right: 0, left: -1400.19 }}
-        >
-          {images.map((image) => (
-            <motion.div className="item">
-              <img id="css" src={image} alt="css" />
-            </motion.div>
-          ))}
+        <motion.div className="container-skills">
+          <motion.div
+            className="slider"
+            drag="x"
+            dragConstraints={{ right: 0, left: -1400.19 }}
+          >
+            {images.map((image) => (
+              <motion.div className="item">
+                <img id="css" src={image} alt="css" />
+              </motion.div>
+            ))}
+          </motion.div>
         </motion.div>
-      </motion.div>
 
-      <div className="btn-right">
-        <a  href='css'>
-          <GrFormNext />
-        </a>
+        <div className="btn-right">
+          <a href="#css">
+            <GrFormNext />
+          </a>
+        </div>
       </div>
     </section>
   );
