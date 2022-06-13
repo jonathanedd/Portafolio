1b04041b8e08cbad7321e82514ae596168be26e1
Jhonatan-Ordonez <jhoeduardonez@gmail.com>
2022-06-12 22:03:06 -0500

    Fixed some queries responsive in skills section

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -4,29 +4,46 @@ import "../skills/skills.css";
 import images from "../exports/images";
 // import names from "../exports/names";
 
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
 import { motion } from "framer-motion";
 
 const Skills = () => {
   // const getNames = names.map((name) => <h5>{name}</h5>);
 
+  // const skillsImg = images.map((image) => <img src={image} alt="Imgs"></img>);
+
+  // const [changeImg, setChangeImg] = useState(skillsImg);
+
   return (
-    <section id="skills">
+    <section id="skills" className="section-skills">
       <h5>Skills that I have</h5>
       <h2>Technologies</h2>
+
+      <div className="btn-left">
+        <a href="#css">
+          <GrFormPrevious />
+        </a>
+      </div>
 
       <motion.div className="container-skills">
         <motion.div
           className="slider"
           drag="x"
-          dragConstraints={{ right: 20, left: -1290.19 }}
+          dragConstraints={{ right: 0, left: -1400.19 }}
         >
           {images.map((image) => (
             <motion.div className="item">
-              <img src={image} alt="" />
+              <img id="css" src={image} alt="css" />
             </motion.div>
           ))}
         </motion.div>
       </motion.div>
+
+      <div className="btn-right">
+        <a  href='css'>
+          <GrFormNext />
+        </a>
+      </div>
     </section>
   );
 };
